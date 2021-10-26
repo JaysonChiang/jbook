165f96a5d0c8e7b15bee27ed735b93c132d13568
JaysonChiang <jayson.chiang@outlook.com>
2021-10-26 21:38:53 +0800

    Draggable Resizing Components

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,3 +1,4 @@
+import './preview.css';
 import { useEffect, useRef } from 'react';
 interface PreviewProps {
   code: string;
@@ -5,7 +6,9 @@ interface PreviewProps {
 
 const html = `
 <html>
-  <head></head>
+  <head>
+  <style>html {backgournd-color: white; }</style>
+  </head>
   <body>
     <div id="root"></div>
     <script>
@@ -28,16 +31,21 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
 
   useEffect(() => {
     iframe.current.srcdoc = html;
-    iframe.current.contentWindow.postMessage(code, '*');
+    setTimeout(() => {
+      iframe.current.contentWindow.postMessage(code, '*');
+    }, 50);
   }, [code]);
 
   return (
-    <iframe
-      title="preview"
-      ref={iframe}
-      sandbox="allow-scripts"
-      srcDoc={html}
-    />
+    <div className="preview-wrapper">
+      <iframe
+        style={{ backgroundColor: 'white' }}
+        title="preview"
+        ref={iframe}
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
+    </div>
   );
 };
 
